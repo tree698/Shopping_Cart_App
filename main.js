1262ab33b6bf5426e14ead52abfa121cbfc80684
Chanwoo <tree698@gmail.com>
2022-05-01 09:04:45 +0900

    display price and its css

--- main.js
+++ main.js
@@ -1,40 +1,52 @@
 'use strict';
 
 const inputItem = document.querySelector('.input__item');
+const inputPrice = document.querySelector('.input__price');
 const form = document.querySelector('.new-form');
 const items = document.querySelector('.items');
 const footerBtn = document.querySelector('.footer__button');
 
 function onAdd() {
   const text = inputItem.value;
+  const price = inputPrice.value;
   if (text === '') {
     inputItem.focus();
     return;
+  } else if (isNaN(price)) {
+    inputPrice.value = '';
+    inputPrice.focus();
+    return;
   }
-  const itemRow = createItem(text);
+
+  const itemRow = createItem(text, price);
   items.appendChild(itemRow);
   itemRow.scrollIntoView({ behavior: 'smooth', block: 'center' });
   inputItem.value = '';
+  inputPrice.value = '';
   inputItem.focus();
+  inputPrice.focus();
 }
 
 let id = 0;
-function createItem(item) {
+function createItem(item, price) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
   itemRow.setAttribute('data-id', id);
   itemRow.innerHTML = `
-    <div class="item">
-      <label class="item__checkbox">
-        <input class="checkbox__default" type="checkbox">
-        <span class="checkbox__new"></span>
-        <span class="item__name">${item}<span>
-      </label>
-      <button class="item__delete" >
-        <i class="fa-solid fa-trash-can" data-id=${id}></i>
-      </button> 
-    </div>
-    <div class="item__divider"></div>`;
+      <div class="item">
+        <label class="item__checkbox">
+          <input class="checkbox__default" type="checkbox">
+          <span class="checkbox__new"></span>
+          <span class="item__name">${item}<span>
+        </label>
+        <div class="wrap__price-delete">
+          <span class="item__price">$${price ? price : 0}</span>
+          <button class="item__delete" >
+            <i class="fa-solid fa-trash-can" data-id=${id}></i>
+          </button> 
+        </div>
+      </div>
+      <div class="item__divider"></div>`;
   ++id;
   return itemRow;
 }
